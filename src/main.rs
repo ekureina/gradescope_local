82f33c50d24e97efc9623bcaf57cb98d5cc3dfee
Claire Moore <nrmoore@uci.edu>
2020-10-17 13:48:55 -0700

    Start writing the GUI
    iced_wgpu is not quite stable, and the opengl reneder using glow has not
    made it's way out of git, so currently using the git repo version

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
+use iced::{Element, Sandbox, Settings, Text};
+
 mod gradescope;
 
-fn main() {
-    println!("Hello, world!");
+struct GSGui;
+
+impl Sandbox for GSGui {
+    type Message = ();
+
+    fn new() -> GSGui {
+        GSGui
+    }
+
+    fn title(&self) -> String {
+        String::from("Gradescope Local")
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        // Boilerplate, no updates currently
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Gradescope Local is under Construction!").into()
+    }
 }
+
+fn main() {
+    GSGui::run(Settings::default());
+} 
