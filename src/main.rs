06475e1b7105a0e09757ee9e36d1036170049d51
Claire Moore <nrmoore@uci.edu>
2020-10-17 22:39:27 -0700

    Refactor current code into it's own class
    Some general cleanup and rearranging to make easier to add new
    functionality, most old code is now in src/results.rs.

--- src/main.rs
+++ src/main.rs
@@ -14,142 +14,99 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-use std::fs;
-use iced::{button, Button, Column, Element, Row, Sandbox, scrollable, Scrollable, Settings, Text, text_input, TextInput};
+use iced::{button, Button, Column, Element, Row, Rule, Sandbox, Settings, Text};
 
 mod gradescope;
+mod results;
 
 #[derive(Debug, Clone)]
-enum GSMessage {
-    RetrieveResults,
-    PathChanged(String)
+enum Message {
+    ChangeState(State),
+    ResultMessage(results::Message)
+}
+
+#[derive(Debug, Clone, Copy)]
+enum State {
+    Home,
+    Visualizer
+}
+
+impl Default for State {
+    fn default() -> State {
+        State::Home
+    }
 }
 
 #[derive(Default)]
 struct GSGui {
-    // The result pulled from 
-    grader_result: Option<gradescope::GraderResult>,
-    // Where to grab the results
-    retrieve_path: String,
+    state: State,
+    visualizer: results::Visualizer,
 
-    // The state of the GUI
-    retrieve_button: button::State,
-    retrieve_path_state: text_input::State,
-    scrollable_state: scrollable::State,
+    home_state: button::State,
+    visualizer_state: button::State
 }
 
 impl Sandbox for GSGui {
-    type Message = GSMessage;
+    type Message = Message;
 
     fn new() -> Self {
         Self::default()
     }
 
     fn title(&self) -> String {
-        String::from("Gradescope Local")
+        let tab = match self.state {
+            State::Home => "Home",
+            State::Visualizer => "Visualizer"
+        };
+        String::from("Gradescope Local: ") + tab
     }
 
     fn update(&mut self, message: Self::Message) {
         match message {
-            Self::Message::RetrieveResults => {
-                self.grader_result = match fs::read(&self.retrieve_path) {
-                    Ok(vec) => {
-                        serde_json::from_slice(vec.as_slice()).ok()
-                    }
-                    Err(_) => {
-                        None
-                    }
-                }
+            Message::ResultMessage(result_message) => {
+                self.visualizer.update(result_message)
             }
-            Self::Message::PathChanged(path) => {
-                self.retrieve_path = path;
+            Message::ChangeState(new_state) => {
+                self.state = new_state
             }
         }
     }
 
-    fn view(&mut self) -> Element<Self::Message> {
-        let results_column: Column<'_, Self::Message> = self.grader_result
-            .as_ref()
-            .map(display_results)
-            .unwrap_or(Column::new().push(Text::new(String::from("No Results Loaded"))));
+    fn view(&mut self) -> Element<Message> {
+        let tab = match self.state {
+            State::Home => {
+                Text::new(String::from("Welcome to Gradescope Local")).into()
+            }
+            State::Visualizer => {
+                self.visualizer
+                    .view()
+                    .map(Message::ResultMessage)
+            }
+        };
+
         Column::new()
-        .push(
-            Text::new("Gradescope Local is under Construction!")
-        )
-        .push(
-            Row::new()
+            .spacing(5)
             .push(
-                TextInput::new(
-                    &mut self.retrieve_path_state,
-                    "Enter the path to Gradescope's output",
-                    &self.retrieve_path,
-                    Self::Message::PathChanged
-                )
-                .on_submit(Self::Message::RetrieveResults)
-            )
-            .push(
-                Button::new(
-                    &mut self.retrieve_button,
-                    Text::new("Retrieve Result")
-                )
-                .on_press(Self::Message::RetrieveResults)
-            )
-        )
-        .push(
-            Scrollable::new(&mut self.scrollable_state)
-            .push(
-                results_column
-            )
-        )
-        .into()
-    }
-}
-
-fn display_results<'a>(result: &gradescope::GraderResult) -> Column<'a, GSMessage> {
-    let score = result.score
-        .unwrap_or_else(
-            || result.tests
-                .as_ref()
-                .unwrap()
-                .iter()
-                .fold(
-                    0.0,
-                    |full_score, test_score| full_score + test_score.score.unwrap()
-                )
-        );
-    let test_output = match result.tests.as_ref() {
-        Some(tests) => { 
-            tests.iter().fold(Column::new().padding(15), |column, test| {
-                let header = test.number.as_ref().unwrap().to_owned() + ") " + test.name.as_ref().unwrap_or(&String::from("")) + "(" +
-                    &test.score.as_ref().unwrap().to_string() + " / " + &test.max_score.as_ref().unwrap().to_string() + ")";
-                column
-                .push(
-                    Column::new()
+                Row::new()
                     .push(
-                        Text::new(header)
-                    )
-                    .push(Text::new(test.output.as_ref().unwrap()))
-                )
-            })
-        }
-        None => {
-            Column::new()
-            .push(Text::new(String::from("No Tests")))
-        }
-    };
-    Column::new().padding(15)
-    .push(
-        Row::new()
-        .push(
-            Text::new(String::from("Score: "))
-        )
-        .push(
-            Text::new(score.to_string())
-        )
-    )
-    .push(
-        test_output
-    )
+                        Button::new(
+                            &mut self.home_state,
+                            Text::new(String::from("Home"))
+                        )
+                        .on_press(Message::ChangeState(State::Home))
+                    )   
+                    .push(
+                        Button::new(
+                            &mut self.visualizer_state,
+                            Text::new(String::from("Visualizer"))
+                        )
+                        .on_press(Message::ChangeState(State::Visualizer))
+                    )   
+            )
+            .push(Rule::horizontal(0))
+            .push(tab)
+            .into()
+    }
 }
 
 fn main() -> std::result::Result<(), iced::Error> {
